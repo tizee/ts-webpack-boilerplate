9ca0134f36ef706c3dcac4b985fe4dcf9947a4f3
tizee <33030965+tizee@users.noreply.github.com>
2020-07-04 23:00:55 +0800

    [update] add bundle-analyzer

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,8 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+// see more options on https://www.npmjs.com/package/webpack-bundle-analyzer
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
   mode: 'development',
@@ -8,4 +11,11 @@ module.exports = merge(common, {
     contentBase: './dist',
     historyApiFallback: true,
   },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'server',
+      analyzerHost: '127.0.0.1',
+      analyzerPort: 'auto',
+    }),
+  ],
 });
